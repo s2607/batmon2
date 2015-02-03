07c3ec7433a536d6e9832c0f6e6ebe75531e2ca7
phreaker2600 <swwiley@gmail.com>
2015-02-03 16:15:13 +0000

    type type type

--- batmon.go
+++ batmon.go
@@ -1,21 +1,41 @@
 package main
 
 import "fmt"
+import "io/ioutil"
 
 type battery struct {
-	num      int
-	name     string
-	path     string
-	capacity int
+	num   int
+	name  string
+	path  string
+	ready bool
+}
+type batmonitor struct {
+	dpath string
 }
 
-func (bat *battery) open() {
-	bat.path = "/sys/" + bat.name
+func (sys batmonitor) getbats() []battery {
+	files, c := ioutil.ReadDir(sys.dpath)
+	fmt.Println(c)
+	bats := make([]battery, len(files))
+	for c, f := range files {
+		fmt.Println(c)
+		bats[c].name = f.Name()
+	}
+	return bats
+}
+func (bat *battery) setpath(sys batmonitor) {
+	if bat.name != "" {
+		bat.path = sys.dpath + bat.name
+	} else {
+
+	}
+	bat.ready = true
 }
 
 func main() {
+	system := batmonitor{dpath: "/sys/class/power_supply/"}
 	curbat := battery{name: "BAT1"}
-	curbat.open()
+	curbat.setpath(system)
 
 	fmt.Println(curbat.path)
 	fmt.Println("hello world")
