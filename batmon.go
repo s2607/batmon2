19b7f97e01ae5604c11a7d9e553a64144543ab74
phreaker2600 <swwiley@gmail.com>
2015-02-03 03:34:04 +0000

    starting to understand

--- batmon.go
+++ batmon.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type battery struct {
+	num      int
+	name     string
+	path     string
+	capacity int
+}
+
+func (bat *battery) open() {
+	bat.path = "/sys/" + bat.name
+}
+
 func main() {
+	curbat := battery{name: "BAT1"}
+	curbat.open()
+
+	fmt.Println(curbat.path)
 	fmt.Println("hello world")
 }
